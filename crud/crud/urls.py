d884b058e6282e56a31883fd6ca8842a7f1943c0
SaRu-N <saruniroula73@gmail.com>
2023-01-16 16:33:45 +0545

    CRUD using Class-based APIView

--- crud/crud/urls.py
+++ crud/crud/urls.py
@@ -2,8 +2,11 @@
 from django.contrib import admin
 from django.urls import path
 from func_based import views as func
+from class_based import views as cl
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('studentapi',func.student_api,name='studentapi'),
+    path('studentapi/',func.student_api,name='studentapi'),
     path('studentapi/<int:pk>/',func.student_api,name='studentapi'),
+    path('studentapicl/',cl.StudentApi.as_view(),name='studentapicl'),
+    path('studentapicl/<int:pk>/',cl.StudentApi.as_view(),name='studentapicl'),
 ]
