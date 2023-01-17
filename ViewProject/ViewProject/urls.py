8eb3b787730b112ca7f7d41257a1dbbf09679c80
SaRu-N <saruniroula73@gmail.com>
2023-01-17 11:16:50 +0545

    Generic API View

---
+++ ViewProject/ViewProject/urls.py
@@ -0,0 +1,16 @@
+
+from django.contrib import admin
+from django.urls import path
+from genericview import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+     path('studentapi/',views.LCStudentAPI.as_view(),name='lcstudentapi'),
+     path('studentapi/<int:pk>/',views.RUDStudentAPI.as_view(),name='rudstudentapi'),
+
+    #  path('studentapi/',views.StudentList.as_view(),name='studentapi'),
+    # path('studentapi/',views.StudentCreate.as_view(),name='studentapi'),
+    # path('studentapi/<int:pk>/',views.StudentRetrieve.as_view(),name='studentapicl'),
+    # path('studentapi/<int:pk>/',views.StudentUpdate.as_view(),name='studentapicl'),
+    # path('studentapi/<int:pk>/',views.StudentDestroy.as_view(),name='studentapicl'),
+]
