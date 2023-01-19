3f0ee950cbd5c828b6b53b974bab7891cdb58ef9
SaRu-N <saruniroula73@gmail.com>
2023-01-19 11:49:56 +0545

    Custom Permission

--- AuthPerProject/basic_auth/views.py
+++ AuthPerProject/basic_auth/views.py
@@ -3,17 +3,18 @@ from .serializers import StudentSerializer
 from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication, SessionAuthentication
 from rest_framework.permissions import IsAuthenticated,IsAdminUser,IsAuthenticatedOrReadOnly,DjangoModelPermissions,DjangoModelPermissionsOrAnonReadOnly
+from .custompermissions import MyPermission
 
 # Session Authentication
-class StudentViewSet(viewsets.ModelViewSet):
-    queryset =Student.objects.all()
-    serializer_class = StudentSerializer
-    authentication_classes = [SessionAuthentication]
-    # permission_classes =[IsAuthenticated]
-    # permission_classes =[IsAuthenticatedOrReadOnly]
-    # permission_classes =[DjangoModelPermissions]
-    permission_classes =[DjangoModelPermissionsOrAnonReadOnly]
-    # permission_classes =[IsAdminUser]
+# class StudentViewSet(viewsets.ModelViewSet):
+#     queryset =Student.objects.all()
+#     serializer_class = StudentSerializer
+#     authentication_classes = [SessionAuthentication]
+#     # permission_classes =[IsAuthenticated]
+#     # permission_classes =[IsAuthenticatedOrReadOnly]
+#     # permission_classes =[DjangoModelPermissions]
+#     permission_classes =[DjangoModelPermissionsOrAnonReadOnly]
+#     # permission_classes =[IsAdminUser]
 
 # Basic Authentication
 # class StudentViewSet(viewsets.ModelViewSet):
@@ -21,3 +22,10 @@ class StudentViewSet(viewsets.ModelViewSet):
 #     serializer_class = StudentSerializer
 #     authentication_classes = [BasicAuthentication]
 #     permission_classes =[IsAuthenticated]
+
+# Custom Permission
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset =Student.objects.all()
+    serializer_class = StudentSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes =[MyPermission]
