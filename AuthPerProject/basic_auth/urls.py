e43f59bd6a3970a393b8e09d40ed6c9248df89ce
SaRu-N <saruniroula73@gmail.com>
2023-01-19 16:17:59 +0545

    Optimize Token Authentication

--- AuthPerProject/basic_auth/urls.py
+++ AuthPerProject/basic_auth/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from . import views
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
+from . auth import CustomAuthToken
 # Creating Router Object
 router =DefaultRouter()
 # Register StudentViewSet with Router
@@ -11,5 +12,6 @@ router.register('studentapi',views.StudentViewSet,basename='student')
 
 urlpatterns = [
    path('',include(router.urls)),
-   path('gettoken/',obtain_auth_token),
+   # path('gettoken/',obtain_auth_token),
+    path('gettoken/', CustomAuthToken.as_view())
 ]
