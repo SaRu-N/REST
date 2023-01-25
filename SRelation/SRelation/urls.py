8a83706d1a376938538105485c40a8ca49a93f78
SaRu-N <saruniroula73@gmail.com>
2023-01-25 16:50:53 +0545

    Nested Serializer

--- SRelation/SRelation/urls.py
+++ SRelation/SRelation/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from api import views
 from myapi import views as myviews
+from nestedapi import views as nviews
 from rest_framework.routers import DefaultRouter
 
 
@@ -10,6 +11,8 @@ router=DefaultRouter()
 
 router.register('singer',views.SingerViewSet,basename='singer')
 router.register('song',views.SongViewSet,basename='song')
+router.register('nsinger',nviews.SingerViewSet,basename='nsinger')
+router.register('nsong',nviews.SongViewSet,basename='nsong')
 router.register('student',myviews.StudentViewSet,basename='student')
 urlpatterns = [
     path('admin/', admin.site.urls),
