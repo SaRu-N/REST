290ea2902bf5ae4f769f5bb5333199ced6d919a6
SaRu-N <saruniroula73@gmail.com>
2023-01-25 11:07:23 +0545

    Search Filter

--- Filter/api/views.py
+++ Filter/api/views.py
@@ -3,12 +3,28 @@ from rest_framework.response import Response
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 # Create your views here.
+
+# class StudentApi(ListAPIView):
+#     queryset=Student.objects.all()
+#     serializer_class =StudentSerializer
+#     # def get_queryset(self):
+#     #     user=self.request.user
+#     #     return Student.objects.filter(enrolled_by=user)
+#     filter_backends =[DjangoFilterBackend]
+#     filterset_fields =['name','city']
 
 class StudentApi(ListAPIView):
     queryset=Student.objects.all()
     serializer_class =StudentSerializer
-    def get_queryset(self):
-        user=self.request.user
-        return Student.objects.filter(enrolled_by=user)
+    # def get_queryset(self):
+    #     user=self.request.user
+    #     return Student.objects.filter(enrolled_by=user)
+    filter_backends =[SearchFilter]
+    search_fields =['name','city']
+    # search_fields =['^name']
+    # search_fields =['=name']
+    
 
