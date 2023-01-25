5e7dfb398edb2e8978d40eb74268ae8aa08bb970
SaRu-N <saruniroula73@gmail.com>
2023-01-25 14:39:32 +0545

    LimitOffset Pagination

--- Pagination/pagenumber/mypagination.py
+++ Pagination/pagenumber/mypagination.py
@@ -1,8 +1,15 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 
-class MyPagination(PageNumberPagination):
-    page_size =3
-    # page_query_param='p'
-    page_size_query_param='records'
-    max_page_size=8
-    last_page_strings = 'end'
+# class MyPagination(PageNumberPagination):
+#     page_size =3
+#     # page_query_param='p'
+#     page_size_query_param='records'
+#     max_page_size=8
+#     last_page_strings = 'end'
+
+class MyPagination(LimitOffsetPagination):
+    default_limit =5
+    # limit_query_param ='mtlimit'
+    # offset_query_param='myoffset'
+    max_limit=7
+
