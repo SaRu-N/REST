d912f7a437e33bfacc0298cf69e4af22281989a4
SaRu-N <saruniroula73@gmail.com>
2023-01-25 16:32:02 +0545

    Hyperlinked ModelSerializer

--- SRelation/SRelation/urls.py
+++ SRelation/SRelation/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from api import views
+from myapi import views as myviews
 from rest_framework.routers import DefaultRouter
 
 
@@ -9,6 +10,7 @@ router=DefaultRouter()
 
 router.register('singer',views.SingerViewSet,basename='singer')
 router.register('song',views.SongViewSet,basename='song')
+router.register('student',myviews.StudentViewSet,basename='student')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(router.urls)),
