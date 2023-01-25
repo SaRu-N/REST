f36a1443970ff5fc5ff1496dc2051f4b675762f3
SaRu-N <saruniroula73@gmail.com>
2023-01-25 11:14:32 +0545

    Ordering Filter

--- Filter/api/views.py
+++ Filter/api/views.py
@@ -4,7 +4,7 @@ from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.generics import ListAPIView
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 # Create your views here.
 
 # class StudentApi(ListAPIView):
@@ -16,15 +16,21 @@ from rest_framework.filters import SearchFilter
 #     filter_backends =[DjangoFilterBackend]
 #     filterset_fields =['name','city']
 
+# Search Filter
 class StudentApi(ListAPIView):
     queryset=Student.objects.all()
     serializer_class =StudentSerializer
-    # def get_queryset(self):
-    #     user=self.request.user
-    #     return Student.objects.filter(enrolled_by=user)
     filter_backends =[SearchFilter]
     search_fields =['name','city']
     # search_fields =['^name']
     # search_fields =['=name']
+
+class StudentApi(ListAPIView):
+    queryset=Student.objects.all()
+    serializer_class =StudentSerializer
+    filter_backends =[OrderingFilter]
+    # ordering_fields =['name']
+    # ordering_fields ='__all__'
+    
     
 
